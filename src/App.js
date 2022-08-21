4722adf4511d918a60ed82f70b25b37aec6110be
Irene Riportella <riportellairene@gmail.com>
2022-08-21 21:39:48 +0200

    created Card component and organized files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Expenses from './components/Expenses';
+import React from 'react';
 
-function App(props) {
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -23,12 +25,19 @@ function App(props) {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />;
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
