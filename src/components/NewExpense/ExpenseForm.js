92b1ef6b0fd836b15dcbbedc9e52058e1b49084d
Irene Riportella <riportellairene@gmail.com>
2022-08-24 16:53:34 +0200

    created NewExpense component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+};
+
+export default ExpenseForm;
