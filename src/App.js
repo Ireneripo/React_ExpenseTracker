320657aa72a48281a9a1cc8de404490a7a70923c
Irene Riportella <riportellairene@gmail.com>
2022-08-25 19:37:00 +0200

    created saveExpenseDataHandler component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -25,6 +25,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -34,8 +39,8 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />;
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 };
