02905857fc5e0f2f9a9d5dc5e7b95e22df89c402
Irene Riportella <riportellairene@gmail.com>
2022-08-31 20:59:29 +0200

    managed form dynamically

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -102,7 +102,9 @@ const ExpenseForm = (props) => {
       </div>
 
       <div className="new-expense__actions">
-        <button type="button">Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
